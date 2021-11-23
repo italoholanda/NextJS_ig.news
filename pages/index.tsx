f6cb923e59d356dbaa31e4096098035a92c7d113
italo <you@example.com>
2021-11-23 13:48:16 -0300

    home inital structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
+import Head from "next/head";
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
-    <h1>
-      
-    </h1>
-  )
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about <br /> the <strong>React world</strong>
+          </h1>
+          <p>
+            Get acess to all News <br /> for <strong>$9.90 month</strong>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
+    </>
+  );
 }
